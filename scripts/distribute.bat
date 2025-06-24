b7002fbfcf918fb8083eea742a4759c14d41b884
Mohamed A.G <moonimooni132@gmail.com>
2025-06-24 08:08:47 +0300

    Add distribution batch file

---
+++ scripts/distribute.bat
@@ -0,0 +1,8 @@
+pushd build 
+
+cmake --build . --config Release --parallel 12 
+xcopy Release\game.exe /f .\
+
+"C:\Program Files\7-Zip\7z.exe" a -tzip "CrossingTheLine.zip" dialogue.txt res\ levels\ game.exe 
+
+popd
